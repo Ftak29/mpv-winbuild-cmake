15443bc862fd280468f58d127afc44820a9fa837
Ftak29 <ftak29@outlook.com>
2026-03-14 18:10:33 -0400

    Refactor mpv build configuration in CMake
    Refactor mpv ExternalProject_Add to include dependencies and remove unnecessary steps.

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -2,29 +2,30 @@ set(MPV_LOCAL_PATCH
     ${CMAKE_CURRENT_LIST_DIR}/patches/mpv-0001-local-cc-hook.patch
 )
 
-DEPENDS
-    angle-headers
-    ffmpeg
-    fribidi
-    lcms2
-    libarchive
-    libass
-    libdvdnav
-    libdvdread
-    libiconv
-    libjpeg
-    libpng
-    luajit
-    rubberband
-    uchardet
-    mujs
-    vulkan
-    shaderc
-    libplacebo
-    spirv-cross
-    vapoursynth
-    libsdl2
-    subrandr
+ExternalProject_Add(mpv
+    DEPENDS
+        angle-headers
+        ffmpeg
+        fribidi
+        lcms2
+        libarchive
+        libass
+        libdvdnav
+        libdvdread
+        libiconv
+        libjpeg
+        libpng
+        luajit
+        rubberband
+        uchardet
+        mujs
+        vulkan
+        shaderc
+        libplacebo
+        spirv-cross
+        vapoursynth
+        libsdl2
+        subrandr
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     GIT_TAG v0.41.0
     SOURCE_DIR ${SOURCE_LOCATION}
@@ -53,6 +54,7 @@ DEPENDS
         -Duchardet=enabled
         -Drubberband=enabled
         -Dlcms2=enabled
+        -Dopenal=disabled
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=enabled
@@ -63,61 +65,3 @@ DEPENDS
     INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
-
-ExternalProject_Add_Step(mpv strip-binary
-    DEPENDEES build
-    ${mpv_add_debuglink}
-    COMMENT "Stripping mpv binaries"
-)
-
-ExternalProject_Add_Step(mpv copy-binary
-    DEPENDEES strip-binary
-
-    # make sure dirs exist (copy won't create them)
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/doc
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/mpv
-
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.exe                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.com                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.com
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.pdf                           ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/doc/manual.pdf
-    COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/etc/fonts/fonts.conf   ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv/fonts.conf
-
-    ${mpv_copy_debug}
-
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libmpv-2.dll                      ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/libmpv-2.dll
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libmpv.dll.a                      ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/libmpv.dll.a
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/include/mpv/client.h              ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/mpv/client.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/include/mpv/stream_cb.h           ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/mpv/stream_cb.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/include/mpv/render.h              ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/mpv/render.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/include/mpv/render_gl.h           ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/mpv/render_gl.h
-
-    COMMENT "Copying mpv binaries and dev files"
-)
-
-
-set(RENAME ${CMAKE_CURRENT_BINARY_DIR}/mpv-prefix/src/rename.sh)
-file(WRITE ${RENAME}
-"#!/bin/bash
-cd $1
-GIT=$(git rev-parse --short=7 HEAD)
-mv $2 $2-git-\${GIT}")
-
-ExternalProject_Add_Step(mpv copy-package-dir
-    DEPENDEES copy-binary
-    COMMAND chmod 755 ${RENAME}
-
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-package ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev ${CMAKE_BINARY_DIR}/mpv-dev-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-dev-${TARGET_CPU}${x86_64_LEVEL}-${BUILDDATE}
-
-    COMMENT "Moving mpv package folder"
-    LOG 1
-)
-
-force_rebuild_git(mpv)
-force_meson_configure(mpv)
-cleanup(mpv copy-package-dir)
-
