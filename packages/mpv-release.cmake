410ee77d37e4e5fe40ccf7f6af4cdb99b76de86a
Ftak29 <ftak29@outlook.com>
2026-03-15 13:59:28 -0400

    Update mpv-release.cmake

--- packages/mpv-release.cmake
+++ packages/mpv-release.cmake
@@ -56,9 +56,8 @@ ExternalProject_Add(mpv-release
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=enabled
-        -Dsubrandr=enabled
         ${mpv_gl}
-        -Dc_args='-Wno-error=int-conversion'
+        -Dc_args=-Wno-error=int-conversion
 
     BUILD_COMMAND ${EXEC} LTO_JOB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
