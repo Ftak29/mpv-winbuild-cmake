c42fa48d81ed7a2f0f9e38c080066ad453fa5a46
Ftak29 <ftak29@outlook.com>
2026-03-23 18:14:37 -0400

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -1,7 +1,5 @@
 set(SOURCE_LOCATION ${SINGLE_SOURCE_LOCATION}/mpv)
-set(MPV_LOCAL_PATCH
-    ${CMAKE_CURRENT_LIST_DIR}/patches/0001-mpv-version.patch
-)
+
 set(mpv_gl -Dgl=enabled)
 
 if(USE_EGL_ANGLE)
@@ -58,7 +56,7 @@ ExternalProject_Add(mpv
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    PATCH_COMMAND git -C <SOURCE_DIR> apply --ignore-space-change --ignore-whitespace ${FFMPEG_LOCAL_PATCH}
+    PATCH_COMMAND /bin/sh -c "VER=$$(cat <SOURCE_DIR>/MPV_VERSION); printf '%s\n' \"EzTvLibW-1.0-$$VER\" > <SOURCE_DIR>/MPV_VERSION"
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=${MINGW_INSTALL_PREFIX}/lib
