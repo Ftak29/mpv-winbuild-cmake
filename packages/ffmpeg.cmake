0b264ef9cdd74588c5b2305791f2a6599265ffad
Ftak29 <ftak29@outlook.com>
2026-03-23 17:49:14 -0400

    Update ffmpeg.cmake

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -50,7 +50,7 @@ ExternalProject_Add(ffmpeg
         rubberband
         libva
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
-    GIT_TAG n8.0.1
+    GIT_TAG n8.1
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
