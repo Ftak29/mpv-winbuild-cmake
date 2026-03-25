d35f982da77b74a0a896025e2f2ea57feab7e398
Ftak29 <ftak29@outlook.com>
2026-03-24 22:21:12 -0400

    Update patch-mpv-version.sh

--- scripts/patch-mpv-version.sh
+++ scripts/patch-mpv-version.sh
@@ -6,7 +6,4 @@ cd "$MPV_SOURCE_DIR"
 base_ver="$(tr -d '\r\n' < MPV_VERSION)"
 printf '%s\n' "${EZTV_LIB_VER}-${base_ver}" > MPV_VERSION
 
-git config user.email "ci@local"
-git config user.name "ci"
-git add MPV_VERSION
-git commit -m "Set custom MPV_VERSION" || true
+echo "Patched MPV_VERSION=$(cat MPV_VERSION)"
