da0911e3edeb28e917aa55763d195adadccf7deb
Ftak29 <ftak29@outlook.com>
2026-03-28 19:56:48 -0400

    Change GIT_TAG from '13.2.1' to 'main'

--- packages/harfbuzz.cmake
+++ packages/harfbuzz.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(harfbuzz
     GIT_REPOSITORY https://github.com/harfbuzz/harfbuzz.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_REMOTE_NAME origin
-    GIT_TAG 13.2.1
+    GIT_TAG main
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !test"
     UPDATE_COMMAND ""
@@ -26,6 +26,6 @@ ExternalProject_Add(harfbuzz
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-#force_rebuild_git(harfbuzz)
+force_rebuild_git(harfbuzz)
 force_meson_configure(harfbuzz)
 cleanup(harfbuzz install)
