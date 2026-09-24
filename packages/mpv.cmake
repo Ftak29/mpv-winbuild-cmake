109178d3571a9c191c9c91afb509c15fb8dacc6f
Ftak29 <ftak29@outlook.com>
2026-09-24 16:00:10 -0400

    Add GIT_TAG for mpv version 0.41.0

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -26,6 +26,7 @@ ExternalProject_Add(mpv
         libsixel
         curl
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
+    GIT_TAG v0.41.0
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
