b156b272302191b2fabd9db2bcf973e94a6dc87a
Ftak29 <ftak29@outlook.com>
2026-09-24 21:48:44 -0400

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -22,14 +22,14 @@ ExternalProject_Add(mpv
         spirv-cross
         vapoursynth
         libsdl2
-        subrandr
         libsixel
-        curl
+
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     GIT_TAG v0.41.0
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
+
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=${MINGW_INSTALL_PREFIX}/lib
@@ -56,10 +56,8 @@ ExternalProject_Add(mpv
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=enabled
-        -Dsubrandr=enabled
         -Dsixel=enabled
         ${mpv_gl}
-        -Dlibcurl=enabled
         -Dc_args='-Wno-error=int-conversion'
     BUILD_COMMAND ${EXEC} LTO_JOB=1 PDB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
